1c52b809da1369bf0bf343d98924b8fc82f50f24
Javi <zenkiu@Javiers-MBP.fios-router.home>
2019-11-12 20:54:27 -0500

    Add render battle players

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -20,7 +20,7 @@ function Instructions () {
             <FaFighterJet className='bg-light' color='#727272' size={140}/>
           </li>
           <li>
-            <h3 className='header-sm'> Enter two Github users </h3>
+            <h3 className='header-sm'> See the winners </h3>
             <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140}/>
           </li>
         </ol>
@@ -86,12 +86,50 @@ PlayerInput.propTypes = {
 
 export default class Battle extends React.Component {
 
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (id, player) {
+
+    this.setState({
+      [id]: player
+    })
+  }
 
   render() {
+
+    const { playerOne, playerTwo } = this.state
+
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput label="label!" onSubmit={(value) => console.log('value', value)}/>
+
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null && (
+              <PlayerInput
+              label='Player One'
+              onSubmit={(player) => this.handleSubmit('playerOne', player)}
+              />
+              )}
+
+            {playerTwo === null && (
+              <PlayerInput
+              label='Player Two'
+              onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+              />
+              )}
+          </div>
+        </div>
       </React.Fragment>
       )
   }
