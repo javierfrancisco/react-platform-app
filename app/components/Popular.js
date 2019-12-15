ccfb276698108fc3db0c3c1a284aca94f2a272d6
Javi <zenkiu@Javiers-MBP.fios-router.home>
2019-12-15 16:50:53 -0500

    Add tooltip

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -4,6 +4,7 @@ import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 import Card from './Card'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 function MenuNav ({ selected, onUpdateOption}) {
   const menuOptions = ['All', 'Java', 'Home', 'Projects', 'Playlists', 'Galleries', 'News']
@@ -46,14 +47,18 @@ function ReposGrid ({repos}) {
               >
                 <ul className='card-list'>
                   <li>
-                    <FaUser color='rgb(255, 191, 116)' size={22} />
-                    <a href={'https://github.com/${login}'}>
-                      {login}
-                    </a>
+                    <Tooltip text='Github Username'>
+                      <FaUser color='rgb(255, 191, 116)' size={22} />
+                      <a href={'https://github.com/${login}'}>
+                        {login}
+                      </a>
+                    </Tooltip>
                   </li>
                   <li>
-                    <FaStar color='rgb(255, 215, 0)' size={22}/>
-                    {stargazers_count.toLocaleString()} stars
+                    <Tooltip text='Users location'>
+                      <FaStar color='rgb(255, 215, 0)' size={22}/>
+                      {stargazers_count.toLocaleString()} stars
+                    </Tooltip>
                   </li>
                   <li>
                     <FaCodeBranch color='rgb(129, 195, 245)' size={22}/>
